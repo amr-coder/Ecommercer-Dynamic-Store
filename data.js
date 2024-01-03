1c6dd55311cf4ba210622077d5bba56ed387ee85
Ambreen BiBi <111267773+amr-coder@users.noreply.github.com>
2024-01-02 22:58:51 -0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let shopItemData= [{
+    id:"rgfh1rtha",
+    name:"Casual Shirt",
+    price:40,
+    desc:"Latest Beautiful Party wear dress In suitable Price",
+    img:"images/c1.jpg"
+ },{
+    id:"rfh2rtha",
+    name:"Office Shirt",
+    price:45,
+    desc:"Latest Beautiful Party wear dress In suitable Price",
+    img:"images/c2.jpg"
+ },{
+    id:"rgh3rtha",
+    name:"T Shirt",
+    price:50,
+    desc:"Latest Beautiful Party wear dress In suitable Price",
+    img:"images/c3.jpg"
+ },{
+    id:"rgf4hrta",
+    name:"Mens Suit",
+    price:450,
+    desc:"Latest Beautiful Party wear dress In suitable Price",
+    img:"images/c4.jpg"
+ },{
+   id:"rgf4hrtb",
+   name:"Mens Suit",
+   price:450,
+   desc:"Latest Beautiful Party wear dress In suitable Price",
+   img:"images/dbw.jpg"
+},{
+   id:"rgf4hrtc",
+   name:"Mens Suit",
+   price:450,
+   desc:"Latest Beautiful Party wear dress In suitable Price",
+   img:"images/dwhite.jpg"
+},{
+   id:"rgf4hrtcd",
+   name:"Mens Suit",
+   price:450,
+   desc:"Latest Beautiful Party wear dress In suitable Price",
+   img:"images/kwhite.jpg"
+},{
+   id:"rgf4hrtde",
+   name:"Mens Suit",
+   price:450,
+   desc:"Latest Beautiful Party wear dress In suitable Price",
+   img:"images/kshirt.jpg"
+}
+];
